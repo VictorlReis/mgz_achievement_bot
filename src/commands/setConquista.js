5504e0012155b94e52a5bd6727ff6d7a9d0bce29
Nicholas Maglowsch Salgueiro <nickmaglowsch3@gmail.com>
2022-05-14 00:27:35 -0300

    implementaçao setConquista

--- src/commands/setConquista.js
+++ src/commands/setConquista.js
@@ -1,9 +1,22 @@
 const Discord = require("discord.js");
+const { validarConquista, validarUsuario, validarParametros } = require('../utils')
 
 
 module.exports.run = async (client, msg, params) => {
-    const doc = "ainda n ta pronto";
+    const output = await setConquista(params);
 
-
-    msg.channel.send(doc);
+    msg.channel.send(output);
 };
+
+
+async function setConquista(params) {
+    const paramValido = validarParametros(params, 2);
+    if (paramValido) return paramValido;
+
+    const [discordTag, conquista, ...xs] = params;
+
+    if (!validarConquista(conquista) || !validarUsuario(discordTag)) return `conquista ou usuário invalidos`;
+
+    Meguinha.findOneAndUpdate({ discordTag }, { $push: { conquistas: conquista } })
+    return `@discordTag agora tem a conquista ${conquista} parabéns!!!`
+}
