71c8bda849b639ba17cfc58f259f10ef60f5be5b
Nicholas Maglowsch Salgueiro <nickmaglowsch3@gmail.com>
2022-05-14 00:35:30 -0300

    implementacao utils

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,34 @@
 const Meguinha = require('./models/meguinha');
+const Conquista = require('./models/conquista');
 
 async function usuarioJaRegistrado(discordTag) {
     return await Meguinha.findOne({ discordTag });
 }
 
+async function conquistaJaRegistrado(nome) {
+    return await Conquista.findOne({ nome });
+}
+
+async function validarUsuario(discordTag) {
+    if (!discordTag) return;
+
+    return usuarioJaRegistrado(discordTag);
+}
+
+async function validarConquista(nome) {
+    if (!nome) return;
+
+    return conquistaJaRegistrado(nome);
+}
+
+function validarParametros(params, numeroEsperado) {
+    if (!params || params.length !== numeroEsperado) return 'parametros invalidos'
+}
+
 module.exports = {
-    usuarioJaRegistrado
+    usuarioJaRegistrado,
+    conquistaJaRegistrado,
+    validarUsuario,
+    validarConquista,
+    validarParametros
 }
