147347e67e340b0befea2c2712656bff07fa6a9d
Nicholas Maglowsch Salgueiro <nickmaglowsch3@gmail.com>
2022-05-15 14:37:40 -0300

    arrumando typo no comando help

--- src/commands/help.js
+++ src/commands/help.js
@@ -8,7 +8,7 @@ module.exports.run = async (client, msg, params) => {
         "Mostrar leaderboard: .a leaderboard\n" +
         "Atribuir uma conquista pra alguém: .a setConquista {DISCORDTAG}/{NOMEDACONQUISTA}\n" +
         "Remover uma conquista de alguém: .a removeConquista {DISCORDTAG} {NOMEDACONQUISTA}\n" +
-        "Atualizar pontuação da conquista: .a updateConquita {NOMEDACONQUISTA}/{PONTUACAO}\n";
+        "Atualizar pontuação da conquista: .a updateConquista {NOMEDACONQUISTA}/{PONTUACAO}\n";
 
     msg.channel.send(doc);
 };
