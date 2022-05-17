cd1b6064eb647f36373358c6bfcfc5a15db48db3
Victor Reis <victorsireis@gmail.com>
2022-05-17 00:05:35 -0300

    Ajustando o leaderboard.

--- src/commands/leaderboard.js
+++ src/commands/leaderboard.js
@@ -4,5 +4,22 @@ const {getAllUsers} = require("../Repositories/UserRepository");
 module.exports.run = async (client, msg, params) => {
     const users = await getAllUsers();
 
-    msg.channel.send(users);
+    const usersView = [];
+    for (let i = 0; i < users.length; i++) {
+        let pontuacaoTotal = 0;
+
+        users[i].conquistas.forEach(c => pontuacaoTotal += c.pontuacao);
+
+        usersView.push({
+            id: users[i].discordId,
+            pontuacaoTotal
+        });
+    }
+
+    var i = 1;
+    usersView.forEach(x => {
+        msg.channel.send(`${i}º <@${x.discordTag}> - ${x.pontuacaoTotal} pontos`);
+        i++;
+    });
+
 };
