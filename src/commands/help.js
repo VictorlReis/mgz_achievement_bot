0c3491986d7520e99be86f9b3d0e83344b7cedee
Nicholas Maglowsch Salgueiro <nickmaglowsch3@gmail.com>
2022-05-17 10:26:46 -0300

    added sincronizar in help

--- src/commands/help.js
+++ src/commands/help.js
@@ -7,7 +7,8 @@ module.exports.run = async (client, msg, _) => {
         "Mostrar leaderboard: .a leaderboard\n" +
         "Atribuir uma conquista pra alguém: .a setConquista {DISCORDTAG}/{NOMEDACONQUISTA}\n" +
         "Remover uma conquista de alguém: .a removeConquista {DISCORDTAG}/{NOMEDACONQUISTA}\n" +
-        "Atualizar pontuação da conquista: .a updateConquista {NOMEDACONQUISTA}/{PONTUACAO}\n";
+        "Atualizar pontuação da conquista: .a updateConquista {NOMEDACONQUISTA}/{PONTUACAO}\n" +
+        "Sincronizar pontuação e conquistas registradas manualmente: .a sincronizar\n";
 
     msg.channel.send(doc);
 };
