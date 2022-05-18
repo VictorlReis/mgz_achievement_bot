83d64c9086f016d6ccbdf4d5a3eb3de4d9432a48
Nicholas Maglowsch Salgueiro <nickmaglowsch3@gmail.com>
2022-05-17 22:03:30 -0300

    implementação updateConquista.js

--- src/Repositories/AchievementRepository.js
+++ src/Repositories/AchievementRepository.js
@@ -1,9 +1,14 @@
 const {Conquista} = require("../models/conquista");
 
 async function findAchievementByName(name) {
-    return Conquista.findOne({ nome: name });
+    return Conquista.findOne({nome: name});
+}
+
+async function updateAchievementByName(name, updatedAchievement) {
+    await Conquista.findOneAndUpdate({nome: name}, {$set: updatedAchievement});
 }
 
 module.exports = {
-    findAchievementByName
+    findAchievementByName,
+    updateAchievementByName
 }
--- src/commands/updateConquista.js
+++ src/commands/updateConquista.js
@@ -1,6 +1,25 @@
+const {validarParametros} = require("../utils");
+const {updateAchievementByName} = require("../Repositories/AchievementRepository");
 
 module.exports.run = async (client, msg, params) => {
-    const doc = "ainda n ta pronto";
+    const output = await updateConquista(params);
 
-    msg.channel.send(doc);
+    msg.channel.send(output);
 };
+
+async function updateConquista(params) {
+    const paramValido = validarParametros(params, 2);
+
+    if (paramValido) return paramValido;
+
+    const [nome, pontuacao] = params;
+
+    try {
+        await updateAchievementByName(nome, {nome, pontuacao});
+    } catch (error) {
+        return `ocorreu um erro liga no devops ${error.message}`;
+    }
+
+    return `Conquista ${nome} atualizada para pontuação ${pontuacao}`
+
+}
