e00fd7b85d3f9d6d085f8626122c4d3be8e0561f
nck98 <105984688+nck98@users.noreply.github.com>
2022-05-21 18:21:52 -0300

    added admin middleware

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const Discord = require('discord.js');
-const { token, connectionString } = require('../config.json');
+const {token, connectionString} = require('../config.json');
 const sequelize = require('./Database');
-const client = new Discord.Client({ intents: ['DIRECT_MESSAGES', 'GUILD_MESSAGES'] });
+const client = new Discord.Client({intents: ['DIRECT_MESSAGES', 'GUILD_MESSAGES']});
+const {adminMiddleware} = require('./middleware/admin.middleware');
 
 client.once('ready', async () => {
     await assertDatabaseConnectionOk();
@@ -10,13 +11,14 @@ client.once('ready', async () => {
 
 client.on("message", async msg => {
 
-    const { command, paramsTratados } = tratarMensagem(msg);
+    const {command, paramsTratados} = tratarMensagem(msg);
 
     if (!command) return;
 
     try {
         const commandFile = require(`./commands/${command}.js`);
-        commandFile.run(client, msg, paramsTratados);
+        const runCmd = () => commandFile.run(client, msg, paramsTratados);
+        adminMiddleware(command, msg, runCmd);
     } catch (err) {
         msg.channel.send("Ta tentando falar comigo? Manda um .a help que eu te ajudo");
         console.log(`(${command}) Error: ${err}`);
@@ -26,7 +28,7 @@ client.on("message", async msg => {
 
 function tratarMensagem(msg) {
 
-    const returnError = { command: undefined };
+    const returnError = {command: undefined};
 
     if (msg.author.bot) return returnError;
     if (msg.channel.type === "dm") return returnError;
@@ -40,7 +42,7 @@ function tratarMensagem(msg) {
 
     const paramsTratados = tratarParams(params);
 
-    return { command, paramsTratados };
+    return {command, paramsTratados};
 }
 
 client.login(token);
---
+++ src/middleware/admin.middleware.js
@@ -0,0 +1,20 @@
+const adminCommands = new Set([
+    "setConquista",
+    "newConquista",
+    "sincronizar",
+    "updateConquista",
+]);
+
+function adminMiddleware(cmd, msg, runCmd) {
+    if(!adminCommands.has(cmd)) return runCmd();
+
+    const isAdmin = msg.member.permissionsIn(msg.channel).has("ADMINISTRATOR");
+
+    if(isAdmin) return runCmd();
+
+    msg.channel.send("Você precisa ser da DIRETORIA pra usar isso ai mané");
+}
+
+module.exports = {
+    adminMiddleware
+}
