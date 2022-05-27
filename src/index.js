d3977e9c3b874a3b5b7fef1eb400d19483ed50eb
Nicholas Maglowsch Salgueiro <nickmaglowsch3@gmail.com>
2022-05-27 10:56:25 -0300

    bug fixs from production

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const sequelize = require('./Database');
 const {Client, Intents} = require('discord.js');
 const {adminMiddleware} = require('./middleware/admin.middleware');
 const {REACTIONS} = require("./constants");
-
+const {getAllRequests} = require('./Repositories/RequestRepository')
 const client = new Client({
     intents: [Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS],
     partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
@@ -20,7 +20,7 @@ async function exec(command, commandType, msg, paramsTratados) {
         const runCmd = () => commandFile.run(client, msg, paramsTratados);
         await adminMiddleware(command, msg, runCmd, paramsTratados.discordId)();
     } catch (err) {
-        msg.channel.send("Ta tentando falar comigo? Manda um .a help que eu te ajudo");
+        msg.channel.send("Ta tentando falar comigo? Manda um !c help que eu te ajudo");
         console.error(`(${command}) Error: ${err}`);
         console.error(err)
     }
@@ -37,7 +37,12 @@ client.on("message", async msg => {
 client.on('messageReactionAdd', async (reaction, user) => {
     if (user.bot) return;
     if(notValidReaction(reaction._emoji.name)) return;
+
     const msg = reaction.message;
+    const requestsMsgIds = (await getAllRequests()).map(request => request.toJSON().idMensagem);
+
+    if(!requestsMsgIds.includes(msg.id)) return;
+
     const params = {
         discordId: user.id,
         reaction: reaction._emoji.name
@@ -78,10 +83,10 @@ function tratarParams(params) {
 function notValidReaction(reaction) {
     for (const reactionKey in REACTIONS) {
         if (reactionKey === reaction) {
-            return true;
+            return false;
         }
     }
-    return false;
+    return true;
 }
 
 async function assertDatabaseConnectionOk() {
@@ -96,4 +101,3 @@ async function assertDatabaseConnectionOk() {
         process.exit(1);
     }
 }
-
